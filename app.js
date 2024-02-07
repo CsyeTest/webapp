7f90384bf01a7287aed392c7cae83940fccad270
nitesh <more.ni@northeastern.edu>
2024-02-07 17:42:52 -0500

    tests implemented

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const config = require("./config/config.js");
 const express = require('express');
-const db = require('./models');
 const routes = require("./routes/index");
 
 
@@ -15,13 +14,4 @@ app.use('*',(req,res)=>{
 res.status(404).send();
 });
 
-app.listen(PORT, async () => {
-    try {
-        await db.sequelize.authenticate();
-        await db.sequelize.sync()
-        console.log('Database connected successfully.');
-        console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
-    } catch (error) {
-        console.error('Error during server startup:', error);
-    }
-})
+module.exports = app;
